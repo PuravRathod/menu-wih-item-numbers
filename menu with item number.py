c2c4734f8dd9f874d4d6dd43ecd2fc0d4148da02
PuravRathod <129208395+PuravRathod@users.noreply.github.com>
2023-03-28 18:50:37 +0100

    Add files via upload

---
+++ menu with item number.py
@@ -0,0 +1,11 @@
+# used example.py as reference
+#  tried giving numbers to food items.
+print ("Order 3 Favourite food items")
+item1 = input (" first item:")
+item2 = input (" second item:")
+item3 = input (" third item:")
+print()
+print ("Order confirmation! You have ordered")
+print ("1-",(item1))
+print ("2-",(item2))
+print ("3-",(item3))
